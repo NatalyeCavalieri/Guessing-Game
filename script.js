aa37131b0cd660f6b0a21fb8edb722f3f8df6cd1
Natalye Cavalieri <nahcavalieri@gmail.com>
2023-08-28 15:12:57 -0300

    init jogo da adivinhação

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
-const randomNumber = Math.round(Math.random() * 10)
+//variaveis
+const btnTry = document.querySelector("#btnTry")
+const btnReset = document.querySelector("#btnReset")
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+
+
+let randomNumber = Math.round(Math.random() * 10)
 let xAttempts =1;
 
-function handleClick (event){
+//eventos
+btnTry.addEventListener('click', handleTryClick)
+btnReset.addEventListener('click', handleResetClick)
+document.addEventListener('keydown', enter)
+
+ 
+
+//função Callback
+function handleTryClick (event){
   event.preventDefault()
+  
 
   const inputNumber = document.querySelector("#inputNumber")
+  const userNumber = Number(inputNumber.value);
 
-if(Number(inputNumber.value == randomNumber)){
-  document.querySelector(".screen1").classList.add("hide")
-  document.querySelector(".screen2").classList.remove("hide")
+  if (isNaN(userNumber) || userNumber < 0 || userNumber > 10) {
+    alert("Por favor, insira um número entre 0 e 10.");
+    return;
+  }
 
+  if (userNumber === randomNumber) {
+    toggleScreen();
+
+  
   document
   .querySelector(".screen2 h2")
   .innerText = `Voce acertou em ${xAttempts} tentativas`
 }
+inputNumber.value = ""
+xAttempts++;
 
-  xAttempts++;
+}
+
   
+
+function handleResetClick(){
+toggleScreen()
+xAttempts=1
+randomNumber = Math.round(Math.random() * 10)
+}
+  
+
+function toggleScreen(){
+screen1.classList.toggle("hide")
+screen2.classList.toggle("hide")
+}
+
+
+function enter (e){
+  if(e.key == 'Enter'&& screen1.classList.contains('hide')){
+    handleResetClick()
+  }
 }
